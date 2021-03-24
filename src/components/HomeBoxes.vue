37dae1fcd5c0fadf45369ca387c4ceb1eda31fbd
cschweda <cschweda@gmail.com>
2021-03-24 17:16:09 -0500

    fix: initial scaffold

---
+++ src/components/HomeBoxes.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container fill-height fluid class="px-0" style="margin-top: -20px">
+    <v-row no-gutters>
+      <v-col cols="12" md="4">
+        <router-link to="/">
+          <v-card
+            dark
+            :color="colors.box1"
+            height="100%"
+            class="elevation-0 px-10 py-8 box"
+          >
+            <h2 class="text-center box-head">Lorem markdownum</h2>
+
+            <v-card-text class="px-2 font-weight-light box-text">
+              Lorem markdownum: tribus me ferum rutilum! Nocte vel hoc, potuisse
+              comitata rigido crudelesque videri quo legit; inquiri. Quodque
+              remis ripa genitus celerem purpureus fuere feris: aevo rident
+              fiducia carpebam introrsus limine.
+            </v-card-text>
+          </v-card>
+        </router-link>
+      </v-col>
+      <v-col cols="12" md="4">
+        <router-link to="/">
+          <v-card
+            dark
+            :color="colors.box2"
+            height="100%"
+            class="elevation-0 px-10 py-8 box"
+          >
+            <h2 class="text-center box-head">Sine qui ferat</h2>
+            <v-card-text class="px-2 font-weight-light box-text">
+              Sine qui ferat, consuescit corpus manibus, phocus! Templo idem
+              procerum. Et semper esset Hersilien flavae; aras ubera postquam
+              deficiunt animus, distulit acta castra anguem. violence.
+            </v-card-text>
+          </v-card>
+        </router-link>
+      </v-col>
+      <v-col cols="12" md="4">
+        <router-link to="/">
+          <v-card
+            dark
+            :color="colors.box3"
+            height="100%"
+            class="elevation-0 px-10 py-8 box"
+          >
+            <h2 class="text-center box-head">Iugalia per inferias</h2>
+            <v-card-text class="px-2 font-weight-light box-text">
+              Iugalia per inferias quid ait omnibus rigorem vivente sumite
+              augere depresso post et astra: mentem. Fieretque Haemonia adicit
+              veniamque talibus, plura est, cur.
+            </v-card-text>
+          </v-card>
+        </router-link>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      colors: {
+        box1: "#aaa",
+        box2: "#999",
+        box3: "#888",
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none !important;
+}
+.box:hover {
+  background: #ccc !important;
+}
+.box:hover > * {
+  color: #444;
+  cursor: pointer;
+}
+.box-head {
+  font-size: 28px;
+  text-transform: uppercase;
+  color: #fff;
+  font-weight: 900;
+}
+.box-text {
+  font-size: 18px;
+  margin-top: 8px;
+  color: #fff;
+}
+.rule {
+  border-bottom: 1px solid #ccc !important;
+  border-top: 1px solid #ccc !important;
+}
+
+.v-sheet.v-card {
+  border-radius: 0px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,349 @@
 <template>
-  <v-container>
-    <v-row> Content here </v-row>
-    <div style="height: 1000px"></div>
-  </v-container>
+  <div style="background: #fafafa">
+    <v-carousel v-model="model" hide-delimiters hide-delimiter-background>
+      <v-carousel-item v-for="(color, index) in colors" :key="index">
+        <v-sheet :color="color" height="100%" tile>
+          <v-row
+            class="fill-height text-center"
+            align="center"
+            justify="center"
+          >
+            <div style="width: 40% !important">
+              <v-container>
+                <v-row>
+                  <v-col cols="12" md="4">
+                    <v-menu top transition="slide-y-transition">
+                      <template v-slot:activator="{ on, attrs }"
+                        ><v-btn v-bind="attrs" v-on="on" large
+                          >Button 1 name here</v-btn
+                        ></template
+                      >
+                      <v-list>
+                        <v-list-item
+                          v-for="(item, index) in items"
+                          :key="index"
+                        >
+                          <v-list-item-title>{{
+                            item.title
+                          }}</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu></v-col
+                  >
+                  <v-col cols="12" md="4">
+                    <v-menu top transition="slide-y-transition">
+                      <template v-slot:activator="{ on, attrs }"
+                        ><v-btn v-bind="attrs" v-on="on" large
+                          >Button 2 name here</v-btn
+                        ></template
+                      >
+                      <v-list>
+                        <v-list-item
+                          v-for="(item, index) in items"
+                          :key="index"
+                        >
+                          <v-list-item-title>{{
+                            item.title
+                          }}</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu></v-col
+                  >
+                  <v-col cols="12" md="4">
+                    <v-menu top transition="slide-y-transition">
+                      <template v-slot:activator="{ on, attrs }"
+                        ><v-btn v-bind="attrs" v-on="on" large
+                          >Button 3 name here</v-btn
+                        ></template
+                      >
+                      <v-list>
+                        <v-list-item
+                          v-for="(item, index) in items"
+                          :key="index"
+                        >
+                          <v-list-item-title>{{
+                            item.title
+                          }}</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu></v-col
+                  >
+                </v-row>
+              </v-container>
+            </div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+    <v-container
+      fluid
+      style="background: #fff"
+      v-if="
+        $vuetify.breakpoint.md ||
+        $vuetify.breakpoint.lg ||
+        $vuetify.breakpoint.xl
+      "
+    >
+      <HomeBoxes></HomeBoxes>
+      <v-card elevation="0" style="border: 1px solid #ccc">
+        <v-toolbar flat color="grey" dark>
+          <v-toolbar-title>Upcoming Events</v-toolbar-title>
+        </v-toolbar>
+        <v-tabs vertical style="background: #eee">
+          <v-tab @click="changeTab('community')"> Community Events</v-tab>
+          <v-tab @click="changeTab('icjia')">ICJIA Events </v-tab>
+          <v-tab @click="changeTab('event3')">Event Type 3</v-tab>
+
+          <v-tab-item>
+            <v-card
+              flat
+              style="background: #eee; height: 300px; overflow-y: auto"
+            >
+              <v-card-text>
+                <p>
+                  Sed aliquam ultrices mauris. Donec posuere vulputate arcu.
+                  Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
+                </p>
+
+                <p>
+                  Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel,
+                  lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis
+                  non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non
+                  nisl sit amet velit hendrerit rutrum.
+                </p>
+
+                <p class="mb-0">
+                  Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu.
+                  Pellentesque libero tortor, tincidunt et, tincidunt eget,
+                  semper nec, quam. Phasellus blandit leo ut odio.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card
+              flat
+              style="background: #eee; height: 300px; overflow-y: auto"
+            >
+              <v-card-text>
+                <p>
+                  Morbi nec metus. Suspendisse faucibus, nunc et pellentesque
+                  egestas, lacus ante convallis tellus, vitae iaculis lacus elit
+                  id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum
+                  aliquam libero, non adipiscing dolor urna a orci. Curabitur
+                  ligula sapien, tincidunt non, euismod vitae, posuere
+                  imperdiet, leo. Nunc sed turpis.
+                </p>
+
+                <p>
+                  Suspendisse feugiat. Suspendisse faucibus, nunc et
+                  pellentesque egestas, lacus ante convallis tellus, vitae
+                  iaculis lacus elit id tortor. Proin viverra, ligula sit amet
+                  ultrices semper, ligula arcu tristique sapien, a accumsan nisi
+                  mauris ac eros. In hac habitasse platea dictumst. Fusce ac
+                  felis sit amet ligula pharetra condimentum.
+                </p>
+
+                <p>
+                  Sed consequat, leo eget bibendum sodales, augue velit cursus
+                  nunc, quis gravida magna mi a libero. Nam commodo suscipit
+                  quam. In consectetuer turpis ut velit. Sed cursus turpis vitae
+                  tortor. Aliquam eu nunc.
+                </p>
+
+                <p>
+                  Etiam ut purus mattis mauris sodales aliquam. Ut varius
+                  tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce
+                  fermentum odio nec arcu.
+                </p>
+
+                <p class="mb-0">
+                  Donec venenatis vulputate lorem. Aenean viverra rhoncus pede.
+                  In dui magna, posuere eget, vestibulum et, tempor auctor,
+                  justo. Fusce commodo aliquam arcu. Suspendisse enim turpis,
+                  dictum sed, iaculis a, condimentum nec, nisi.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card
+              flat
+              style="background: #eee; height: 300px; overflow-y: auto"
+            >
+              <v-card-text>
+                <p>
+                  Fusce a quam. Phasellus nec sem in justo pellentesque
+                  facilisis. Nam eget dui. Proin viverra, ligula sit amet
+                  ultrices semper, ligula arcu tristique sapien, a accumsan nisi
+                  mauris ac eros. In dui magna, posuere eget, vestibulum et,
+                  tempor auctor, justo.
+                </p>
+
+                <p class="mb-0">
+                  Cras sagittis. Phasellus nec sem in justo pellentesque
+                  facilisis. Proin sapien ipsum, porta a, auctor quis, euismod
+                  ut, mi. Donec quam felis, ultricies nec, pellentesque eu,
+                  pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
+      <div style="font-size: 12px" class="mt-2 text-right">
+        Shrink browser window to see mobile view
+      </div>
+    </v-container>
+    <v-container fluid style="background: #fff" v-else>
+      <v-card elevation="0" style="border: 1px solid #ccc">
+        <v-toolbar flat color="grey" dark>
+          <v-toolbar-title>Upcoming Events</v-toolbar-title>
+        </v-toolbar>
+        <v-tabs style="background: #eee" center-active>
+          <v-tab @click="changeTab('community')"> Community Events</v-tab>
+          <v-tab @click="changeTab('icjia')">ICJIA Events </v-tab>
+          <v-tab @click="changeTab('event3')">Event Type 3</v-tab>
+
+          <v-tab-item>
+            <v-card
+              flat
+              style="background: #eee; height: 300px; overflow-y: auto"
+            >
+              <v-card-text>
+                <p>
+                  Sed aliquam ultrices mauris. Donec posuere vulputate arcu.
+                  Morbi ac felis. Etiam feugiat lorem non metus. Sed a libero.
+                </p>
+
+                <p>
+                  Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel,
+                  lacus. Aenean tellus metus, bibendum sed, posuere ac, mattis
+                  non, nunc. Aliquam lobortis. Aliquam lobortis. Suspendisse non
+                  nisl sit amet velit hendrerit rutrum.
+                </p>
+
+                <p class="mb-0">
+                  Phasellus dolor. Fusce neque. Fusce fermentum odio nec arcu.
+                  Pellentesque libero tortor, tincidunt et, tincidunt eget,
+                  semper nec, quam. Phasellus blandit leo ut odio.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card
+              flat
+              style="background: #eee; height: 300px; overflow-y: auto"
+            >
+              <v-card-text>
+                <p>
+                  Morbi nec metus. Suspendisse faucibus, nunc et pellentesque
+                  egestas, lacus ante convallis tellus, vitae iaculis lacus elit
+                  id tortor. Sed mollis, eros et ultrices tempus, mauris ipsum
+                  aliquam libero, non adipiscing dolor urna a orci. Curabitur
+                  ligula sapien, tincidunt non, euismod vitae, posuere
+                  imperdiet, leo. Nunc sed turpis.
+                </p>
+
+                <p>
+                  Suspendisse feugiat. Suspendisse faucibus, nunc et
+                  pellentesque egestas, lacus ante convallis tellus, vitae
+                  iaculis lacus elit id tortor. Proin viverra, ligula sit amet
+                  ultrices semper, ligula arcu tristique sapien, a accumsan nisi
+                  mauris ac eros. In hac habitasse platea dictumst. Fusce ac
+                  felis sit amet ligula pharetra condimentum.
+                </p>
+
+                <p>
+                  Sed consequat, leo eget bibendum sodales, augue velit cursus
+                  nunc, quis gravida magna mi a libero. Nam commodo suscipit
+                  quam. In consectetuer turpis ut velit. Sed cursus turpis vitae
+                  tortor. Aliquam eu nunc.
+                </p>
+
+                <p>
+                  Etiam ut purus mattis mauris sodales aliquam. Ut varius
+                  tincidunt libero. Aenean viverra rhoncus pede. Duis leo. Fusce
+                  fermentum odio nec arcu.
+                </p>
+
+                <p class="mb-0">
+                  Donec venenatis vulputate lorem. Aenean viverra rhoncus pede.
+                  In dui magna, posuere eget, vestibulum et, tempor auctor,
+                  justo. Fusce commodo aliquam arcu. Suspendisse enim turpis,
+                  dictum sed, iaculis a, condimentum nec, nisi.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item>
+            <v-card
+              flat
+              style="background: #eee; height: 300px; overflow-y: auto"
+            >
+              <v-card-text>
+                <p>
+                  Fusce a quam. Phasellus nec sem in justo pellentesque
+                  facilisis. Nam eget dui. Proin viverra, ligula sit amet
+                  ultrices semper, ligula arcu tristique sapien, a accumsan nisi
+                  mauris ac eros. In dui magna, posuere eget, vestibulum et,
+                  tempor auctor, justo.
+                </p>
+
+                <p class="mb-0">
+                  Cras sagittis. Phasellus nec sem in justo pellentesque
+                  facilisis. Proin sapien ipsum, porta a, auctor quis, euismod
+                  ut, mi. Donec quam felis, ultricies nec, pellentesque eu,
+                  pretium quis, sem. Nam at tortor in tellus interdum sagittis.
+                </p>
+              </v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
-  name: "Home",
+  data() {
+    return {
+      model: 0,
+      colors: [
+        "grey lighten-1",
+        "grey darken-2",
+        "grey lighten-1",
+        "grey darken-2",
+      ],
+      items: [
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me" },
+        { title: "Click Me 2" },
+      ],
+    };
+  },
+  mounted() {
+    this.fixA11y();
+  },
+  methods: {
+    changeTab(e) {
+      console.log(e);
+    },
+    fixA11y() {
+      window.$("i").replaceTagName("span");
+    },
+  },
 };
 </script>
+
+<style>
+.v-tab {
+  justify-content: left !important;
+}
+.v-tab--active {
+  color: #000 !important;
+}
+</style>
