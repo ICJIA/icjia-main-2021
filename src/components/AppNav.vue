28c9b8119398802fb78fa4a36b3c3adef0baa139
cschweda <cschweda@gmail.com>
2021-03-25 08:43:34 -0500

    fix: navBar

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <v-app-bar app>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
-      <div class="d-flex align-center">
+    <v-app-bar fixed app color="white" hide-on-scroll>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <div>
         <v-img
           alt="ICJIA Logo"
           class="shrink mr-2 hover"
@@ -29,18 +29,122 @@
       >
         ILLINOIS CRIMINAL JUSTICE INFORMATION AUTHORITY
       </div>
-      <!-- {{ $store.state.auth.isAuthenticated }} -->
+
       <v-spacer></v-spacer>
 
-      <v-btn icon aria-label="search">
-        <v-icon>mdi-magnify</v-icon>
+      <v-menu
+        bottom
+        offset-y
+        origin="center center"
+        transition="scale-transition"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text class="hidden-sm-and-down" v-bind="attrs" v-on="on"
+            >Item 1<v-icon right small>arrow_drop_down</v-icon>
+          </v-btn>
+        </template>
+        <v-list nav dense elevation="2">
+          <v-list-item class="appNav">
+            <v-list-item-content class="hover">
+              <v-list-item-title style="font-size: 12px !important"
+                >Sub Item 1</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item class="appNav">
+            <v-list-item-content class="hover">
+              <v-list-item-title style="font-size: 12px !important"
+                >Sub Item 2</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+          <v-list-item class="appNav" target="_blank">
+            <v-list-item-content class="hover">
+              <v-list-item-title style="font-size: 12px !important"
+                >Sub Item 3</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item class="appNav" target="_blank">
+            <v-list-item-content class="hover">
+              <v-list-item-title style="font-size: 12px !important"
+                >Sub Item 4</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-btn text class="hidden-sm-and-down navItem">Item 2</v-btn>
+
+      <v-btn text class="hidden-sm-and-down navItem">Item 3</v-btn>
+
+      <v-btn text class="hidden-sm-and-down navItem">Item 4</v-btn>
+
+      <v-menu
+        bottom
+        offset-y
+        origin="center center"
+        transition="scale-transition"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text class="hidden-sm-and-down" v-bind="attrs" v-on="on"
+            >Item 5<v-icon right small>arrow_drop_down</v-icon>
+          </v-btn>
+        </template>
+        <v-list nav dense elevation="2">
+          <v-list-item class="appNav">
+            <v-list-item-content class="hover">
+              <v-list-item-title style="font-size: 12px !important"
+                >Sub Item 1</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item class="appNav">
+            <v-list-item-content class="hover">
+              <v-list-item-title style="font-size: 12px !important"
+                >Sub Item 2</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider></v-divider>
+          <v-list-item class="appNav" target="_blank">
+            <v-list-item-content class="hover">
+              <v-list-item-title style="font-size: 12px !important"
+                >Sub Item 3</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-btn icon>
+        <span class="v-icon mdi mdi-magnify"></span>
       </v-btn>
+
+      <!-- <v-btn icon>
+        <span class="v-icon mdi mdi-dots-vertical"></span>
+      </v-btn> -->
     </v-app-bar>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    // window.$("i").replaceTagName("span");
+    // window.$('button[role="button"]').removeRedundantAttributes("role");
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.navItem {
+  color: #000 !important;
+}
+</style>
