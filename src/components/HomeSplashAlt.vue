d5f40ae026f3940b590f6788bb5bc9ff30686412
cschweda <cschweda@gmail.com>
2021-04-03 12:25:14 -0500

    fix: overlay opacity

--- src/components/HomeSplashAlt.vue
+++ src/components/HomeSplashAlt.vue
@@ -10,12 +10,12 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="blue lighten-3">
             <div
               class="text-center px-5"
-              style="background: rgba(125, 125, 125, 0.7); padding: 25px"
+              style="background: rgba(125, 125, 125, 0.8); padding: 25px"
               width="100% !important"
             >
               <div class="text-center px-5" style="min-width: 350px">
@@ -35,12 +35,12 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="red lighten-3">
             <div
               class="text-center px-5"
-              style="background: rgba(125, 125, 125, 0.9); padding: 25px"
+              style="background: rgba(125, 125, 125, 0.8); padding: 25px"
               width="100% !important"
               min-height="200"
             >
@@ -64,7 +64,7 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="blue lighten-3">
             <div
@@ -106,12 +106,12 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="blue lighten-3">
             <div
               class="text-center px-5"
-              style="background: rgba(125, 125, 125, 0.7); padding: 25px"
+              style="background: rgba(125, 125, 125, 0.9); padding: 25px"
               width="100% !important"
             >
               <div class="text-center px-5" style="min-width: 350px">
@@ -131,7 +131,7 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="red lighten-3">
             <div
@@ -157,7 +157,7 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="blue lighten-3">
             <div
@@ -191,7 +191,7 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="blue lighten-3">
             <div
@@ -221,12 +221,12 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".3" color="red lighten-3">
             <div
               class="text-center px-5"
-              style="background: rgba(125, 125, 125, 0.9); padding: 25px"
+              style="background: rgba(125, 125, 125, 0.7); padding: 25px"
               width="100% !important"
               min-height="200"
             >
@@ -256,12 +256,12 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".5">
             <div
               class="text-center px-5"
-              style="background: rgba(50, 50, 50, 0.9); padding: 25px"
+              style="background: rgba(50, 50, 50, 0.8); padding: 25px"
               width="100% !important"
               min-height="200"
             >
@@ -289,12 +289,12 @@
           "
           aspect-ratio="1"
           class="grey lighten-2"
-          max-height="400"
+          max-height="450"
         >
           <v-overlay absolute opacity=".5">
             <div
               class="text-center px-5"
-              style="background: rgba(50, 50, 50, 0.9); padding: 25px"
+              style="background: rgba(50, 50, 50, 0.8); padding: 25px"
               width="100% !important"
               min-height="200"
             >
@@ -398,6 +398,129 @@
                         <v-btn outlined class="mr-3">Button here</v-btn>
                       </v-col></v-row
                     ></v-container
+                  >
+                </div>
+              </div>
+            </div>
+          </v-overlay>
+        </v-img>
+      </v-col>
+    </v-row>
+    <h2 class="ml-5">Version H (Monochrome only)</h2>
+    <v-row no-gutters>
+      <v-col cols="12" md="4">
+        <v-img
+          :src="getImagePath('https://dev.icjia.cloud/chicago-03-min-bw.jpg')"
+          :lazy-src="
+            getImagePath(
+              'https://dev.icjia.cloud/chicago-03-min-bw.jpg',
+              0,
+              0,
+              10
+            )
+          "
+          aspect-ratio="1"
+          class="grey lighten-2"
+          max-height="500"
+        >
+          <v-overlay absolute opacity=".3">
+            <div
+              class="text-center px-5"
+              style="background: rgba(36, 92, 150, 0.8); padding: 25px"
+              width="100% !important"
+            >
+              <div class="text-center px-5" style="min-width: 350px">
+                <h1 class="nofo-title mt-3" style="color: #fff">
+                  Title 1 here
+                </h1>
+                <div class="nofo-tagline">Short summary here</div>
+                <div class="mt-4">
+                  <v-container fluid
+                    ><v-row>
+                      <v-col cols="12" md="12">
+                        <v-btn outlined class="mr-3">Button here</v-btn>
+                      </v-col>
+                    </v-row></v-container
+                  >
+                </div>
+              </div>
+            </div>
+          </v-overlay>
+        </v-img>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-img
+          :src="getImagePath('https://dev.icjia.cloud/chicago-01-min-bw.jpg')"
+          :lazy-src="
+            getImagePath(
+              'https://dev.icjia.cloud/chicago-01-min-bw.jpg',
+              0,
+              0,
+              10
+            )
+          "
+          aspect-ratio="1"
+          class="grey lighten-2"
+          max-height="500"
+        >
+          <v-overlay absolute opacity=".3">
+            <div
+              class="text-center px-5"
+              style="background: rgba(36, 92, 150, 0.8); padding: 25px"
+              width="100% !important"
+            >
+              <div class="text-center px-5" style="min-width: 350px">
+                <h1 class="nofo-title mt-3" style="color: #fff">
+                  Title 1 here
+                </h1>
+                <div class="nofo-tagline">Short summary here</div>
+                <div class="mt-4">
+                  <v-container fluid
+                    ><v-row>
+                      <v-col cols="12" md="12">
+                        <v-btn outlined class="mr-3">Button here</v-btn>
+                      </v-col>
+                    </v-row></v-container
+                  >
+                </div>
+              </div>
+            </div>
+          </v-overlay>
+        </v-img>
+      </v-col>
+      <v-col cols="12" md="4">
+        <v-img
+          :src="getImagePath('https://dev.icjia.cloud/chicago-02-min-bw.jpg')"
+          :lazy-src="
+            getImagePath(
+              'https://dev.icjia.cloud/chicago-02-min-bw.jpg',
+              0,
+              0,
+              10
+            )
+          "
+          aspect-ratio="1"
+          class="grey lighten-2"
+          max-height="500"
+        >
+          <v-overlay absolute opacity=".3">
+            <div
+              class="text-center px-5"
+              style="background: rgba(36, 92, 150, 0.8); padding: 25px"
+              width="100% !important"
+            >
+              <div class="text-center px-5" style="min-width: 350px">
+                <h1 class="nofo-title mt-3" style="color: #fff">
+                  Title 1 here
+                </h1>
+                <div class="nofo-tagline">Short summary here</div>
+                <div class="mt-4">
+                  <v-container fluid
+                    ><v-row>
+                      <v-col cols="12" md="12">
+                        <v-btn outlined class="mr-3">Button here</v-btn>
+                      </v-col>
+                    </v-row></v-container
                   >
                 </div>
               </div>
